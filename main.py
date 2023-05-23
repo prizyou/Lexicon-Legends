900ff71c60c81af8e91118320def689536d155c9
mAx <62607103+maxh7@users.noreply.github.com>
2023-05-23 16:57:02 +0200

    deit

--- main.py
+++ main.py
@@ -1,36 +1,161 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn as sk
+import streamlit as st
 
-st.title('Fahrverhalten: Routen zur Universität')
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import TimeSeriesSplit, train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
-DATE_COLUMN = 'time'
-DATA = ('bike_ride.json')
+st.title('Sturzerkennung')
+
+# Feld für Drag&Drop fuer Testdaten
 
 data_load_state = st.text('Loading data...')
 
-df_example = pd.read_json(DATA)
+df_walk = pd.read_json("normal_walk.json")
+
+df_fall = pd.read_json("fall.json")
+
+df_testFall = pd.read_json("test_fall.json")
+
 
 #preprocessing data
-df_example['time'] = pd.to_datetime(df_example['time'])
-df_example = df_example.set_index('time')
-df_example['sensor'].unique()
+df_walk['time'] = pd.to_datetime(df_walk['time'])
+df_fall['time'] = pd.to_datetime(df_fall['time'])
+df_testFall['time'] = pd.to_datetime(df_testFall['time'])
+
+df_walk = df_walk.set_index('time')
+df_fall = df_fall.set_index('time')
+df_testFall = df_testFall.set_index('time')
+
+df_walk['sensor'].unique()
+df_walk_acc = df_walk[df_walk['sensor'] == 'AccelerometerUncalibrated']
+df_fall_acc = df_fall[df_fall['sensor'] == 'AccelerometerUncalibrated']
+df_testFall_acc = df_testFall[df_testFall['sensor'] == 'AccelerometerUncalibrated']
+
+df_walk_gyro = df_walk[df_walk['sensor'] == 'GyroscopeUncalibrated']
+df_fall_gyro = df_fall[df_fall['sensor'] == 'GyroscopeUncalibrated']
+df_testFall_gyro = df_testFall[df_testFall['sensor'] == 'GyroscopeUncalibrated']
+
+df_walk_or = df_walk[df_walk['sensor'] == 'Orientation']
+df_fall_or = df_fall[df_fall['sensor'] == 'Orientation']
+df_testFall_or = df_testFall[df_testFall['sensor'] == 'Orientation']
 
 
-df_example_acc = df_example[df_example['sensor'] == 'AccelerometerUncalibrated']
 
-st.write(df_example_acc.head(5))
+df_walk_acc = df_walk_acc[['z','x','y']]
+df_fall_acc = df_fall_acc[['z','x','y']]
+df_testFall_acc = df_testFall_acc[['z','x','y']]
 
-if st.checkbox('Include x,y Data'):
-    st.subheader('Raw data')
-    df_example_acc = df_example_acc[['z','x','y']]
-    st.line_chart(data=df_example_acc)
+df_walk_gyro = df_walk_gyro[['z','x','y']]
+df_fall_gyro = df_fall_gyro[['z','x','y']]
+df_testFall_gyro = df_testFall_gyro[['z','x','y']]
+
+df_walk_or = df_walk_or[['qx','qz','qw','qy']]
+df_fall_or = df_fall_or[['qx','qz','qw','qy']]
+df_testFall_or = df_testFall_or[['qx','qz','qw','qy']]
+
+df_walk_gyro.rename(columns={ 'z': 'gz' , 'x': 'gx' , 'y': 'gy'}, inplace=True)
+df_fall_gyro.rename(columns={ 'z': 'gz' , 'x': 'gx' , 'y': 'gy'}, inplace=True)
+
+df_walk_acc.plot(figsize=(10,5))
+df_fall_acc.plot(figsize=(10,5))
+df_testFall_acc.plot(figsize=(10,5))
+df_walk_gyro.plot(figsize=(10,5))
+df_fall_gyro.plot(figsize=(10,5))
 
 
-st.subheader('Aufgezeichnete Bewegungen über die Fahrt')
-st.write('z-Achse Ausschlag vertikal, x,y Ausschlag horizontal')
+df_walk_acc['label'] = "walk"
+df_fall_acc['label'] = "fall"
 
-df_example_acc = df_example_acc['z']
+df_walk_gyro['label'] = "walk"
+df_fall_gyro['label'] = "fall"
 
-st.line_chart(data=df_example_acc)
+df_walk_or['label'] = "walk"
+df_fall_or['label'] = "fall"
+
+df_testFall_acc['label'] = "fall"
+df_testFall_gyro['label'] = "fall"
+df_testFall_or['label'] = "fall"
+
+df_combined_walk = pd.merge(df_walk_acc, df_walk_gyro, on='time')
+df_combined_fall = pd.merge(df_fall_acc, df_fall_gyro, on='time')
+
+
+df_combined_walk.drop('label_x',axis=1, inplace=True)
+df_combined_fall.drop('label_x',axis=1, inplace=True)
+
+df_combined_walk['id'] = 0
+df_combined_fall['id'] = 0
+
+id = 1
+
+var1 = 100
+var2 = 100
+
+for i in range(0, len(df_combined_walk)):
+    df_combined_walk.iloc[i,7] = id
+    
+
+    if i >= var1: 
+        var1 = var1 + 100
+        id+=1
+    
+for i in range(0, len(df_combined_fall)):
+    
+    df_combined_fall.iloc[i,7] = id
+
+    if i >= var2: 
+        var2 = var2 + 100
+        id+=1
+
+
+df_combined = pd.concat([df_combined_walk, df_combined_fall])
+
+df_help= df_combined.set_index('time')
+df_help[['z','x','y']].plot(figsize=(10,10))
+
+df_combined.isnull().sum()
+df_combined.reset_index(inplace=True)
+
+df_combined.replace(['walk'],0,inplace=True)
+df_combined.replace(['fall'],1,inplace=True)
+
+from tsfresh import extract_features
+extracted_features = extract_features(df_combined, column_id="id",column_sort='time')
+extracted_features
+labels= df_combined[['id','label_y']]
+labels= labels.drop_duplicates()
+labels= labels.set_index('id')
+y= labels.iloc[:,0]
+
+
+
+from tsfresh import select_features
+from tsfresh.utilities.dataframe_functions import impute
+
+impute(extracted_features)
+features_filtered = select_features(extracted_features,y)
+from tsfresh import extract_relevant_features
+
+features_filtered_direct = extract_relevant_features(df_combined, y,
+                                                     column_id='id', column_sort='time')
+features_filtered_direct
+#Split Data 
+#X_train, X_test, y_train, y_test = train_test_split(df_combined.drop('label_y',axis=1), df_combined['label_y'], test_size=0.30, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(features_filtered_direct, y, test_size=0.30, random_state=101)
+X_test
+y_test
+#KNN
+knn = KNeighborsClassifier(n_neighbors=3,weights='distance')
+knn.fit(X_train,y_train)
+
+y_pred = knn.predict(X_test)
+#Test via Konfusionsmatrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
 
