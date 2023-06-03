7347b028fa79843ad938731c64950c8597abda0b
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-03 15:53:44 +0200

    edit

--- streamlit.py
+++ streamlit.py
@@ -105,11 +105,12 @@ if uploaded_file is not None:
         data_combine = data_combine.reset_index(inplace=False)
         
         features_filtered_direct = extract_features(data_combine,column_id='id', column_sort='time')
+        
+        st.write("Extrahierte Features:")
         st.write(features_filtered_direct)
 
         #Vortrainierte Modelle laden
         model_knn = pk.load(open('knnpickle_file','rb'),)
-        st.write("hoer")
 
         #Schätzungsdaten rausziehen
         y_pred_knn = model_knn.predict(features_filtered_direct)
