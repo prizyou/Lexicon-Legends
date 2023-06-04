ff7f3c0aba29ad56c5d2aa50151101940a6ed481
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-04 09:59:49 +0200

    edit

--- streamlit.py
+++ streamlit.py
@@ -18,7 +18,7 @@ st.set_page_config(
 )
 
 st.title("Sturzerkennun:")
-st.sidebar.success("Klicke dich durch die Applikationsdokumentation")
+st.sidebar.success("Klicke durch die Dokumentation")
 
 # Feld für Drag&Drop fuer Testdaten
 uploaded_file = st.file_uploader("Choose a file as .JSON")
