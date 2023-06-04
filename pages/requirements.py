e158db023468834eecf4c9fb5a0066ea8495d608
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-04 09:55:55 +0200

    edit

---
+++ pages/requirements.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Requirements:")
+st.write("The following requirements are needed to run the application:")
+st.write("1. Python 3.7.9")
+st.write("2. Streamlit 0.79.0")
+st.write("3. Pandas 1.2.3")
+st.write("4. Numpy 1.20.1")
+st.write("5. Scikit-learn 0.24.1")
+st.write("6. Matplotlib 3.3.4")
