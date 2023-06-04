4e2f7d64778f02228ab770c3c5505e052694e979
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-04 10:10:25 +0200

    edit

--- pages/Theorie.py
+++ pages/Theorie.py
@@ -1,4 +1,44 @@
 import streamlit as st 
 
 st.title("Theoretische Überlegungen hinter dem Modell:")
-st.write("")
+st.write("""Project Focus: Design a Application for elderly people to detect fall behavior 
+
+FaLL Detection for elderly people: 
+
+GOAL for app deployment is to increase accuracy in fall detection models by comparing different models 
+
+Documentation links to literature: 
+
+Fall Detection via Machine Learning: 
+
+https://www.researchgate.net/publication/308467199_Fall_Detection_Using_Machine_Learning_Algorithms
+
+=> The k-NN  were the
+    most reliable system (evidence from paper above)
+    
+ Activity Labels: 
+ 
+- Standing
+- Walking
+- Jogging
+- Jumping
+- Stairs (UP)
+- Stairs (DOWN)
+- Sitting
+    
+https://www.sciencedirect.com/science/article/pii/S1877050918318398
+
+https://kth.diva-portal.org/smash/get/diva2:1230962/FULLTEXT01.pdf
+
+https://www.researchgate.net/publication/353576862_Latest_Research_Trends_in_Fall_Detection_and_Prevention_Using_Machine_Learning_A_Systematic_Review
+
+Entwurf der Streamlit APP: Auswahl (Dropdown Menue zwischen 2-3 verschiedenen Modellen, womit man die Aktivitaeten erkennen kann) -> Label Fall vorhanden oder nicht (eventuell dann auch mit der Zeitangabe, wann dieser Fall vorgelegen hat)
+
+Zunaechst Datei Import ermoeglichen mit Kachel Drag&Drop Funktion
+
+Threshold Decision Tree recherchieren!: 
+
+=>>>>>>> https://github.com/ignaciocordova/Online-Fall-Detection-Python/blob/main/A%20smartphone-based%20online%20system%20for%20fall%20detection%20with%20alert%20notifications%20and%20contextual%20information%20of%20real-life%20falls.pdf
+
+https://jneuroengrehab.biomedcentral.com/articles/10.1186/s12984-021-00918-z
+""")
