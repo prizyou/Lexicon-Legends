ed4841a96f283d43594e10a430147352448f47a5
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-04 10:11:42 +0200

    edit

--- pages/Theorie.py
+++ pages/Theorie.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Theoretische Überlegungen hinter dem Modell:")
 st.write("""Project Focus: Design a Application for elderly people to detect fall behavior 
 
-FaLL Detection for elderly people: 
+FaLLDetector for elderly people: 
 
 GOAL for app deployment is to increase accuracy in fall detection models by comparing different models 
 
@@ -32,13 +32,7 @@ https://kth.diva-portal.org/smash/get/diva2:1230962/FULLTEXT01.pdf
 
 https://www.researchgate.net/publication/353576862_Latest_Research_Trends_in_Fall_Detection_and_Prevention_Using_Machine_Learning_A_Systematic_Review
 
-Entwurf der Streamlit APP: Auswahl (Dropdown Menue zwischen 2-3 verschiedenen Modellen, womit man die Aktivitaeten erkennen kann) -> Label Fall vorhanden oder nicht (eventuell dann auch mit der Zeitangabe, wann dieser Fall vorgelegen hat)
-
 Zunaechst Datei Import ermoeglichen mit Kachel Drag&Drop Funktion
-
-Threshold Decision Tree recherchieren!: 
-
-=>>>>>>> https://github.com/ignaciocordova/Online-Fall-Detection-Python/blob/main/A%20smartphone-based%20online%20system%20for%20fall%20detection%20with%20alert%20notifications%20and%20contextual%20information%20of%20real-life%20falls.pdf
 
 https://jneuroengrehab.biomedcentral.com/articles/10.1186/s12984-021-00918-z
 """)
