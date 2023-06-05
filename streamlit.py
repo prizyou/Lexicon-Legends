a3106caf5c60174a8f8f8c642a4f96ec9688a25c
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-05 22:48:10 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -148,7 +148,7 @@ if uploaded_file is not None:
         st.caption("In "+ str(y_pred_knn.sum()) + " der übertragenen " + str(data_combine['id'].unique().max()) + " Sequenzen aus der Aufzeichnung liegt vermutlich ein Sturz vor")
         
         y_pred_knn = pd.DataFrame(y_pred_knn)
-        y_pred_knn.replace(to_replace=0, value="No Fall", inplace=True)
+        y_pred_knn.replace(to_replace=0, value="Normal", inplace=True)
         y_pred_knn.replace(to_replace=1, value="Fall", inplace=True)
         
         st.write(y_pred_knn)
@@ -157,7 +157,7 @@ if uploaded_file is not None:
         st.caption("In "+ str(y_pred_rf.sum()) + " der übertragenen " + str(data_combine['id'].unique().max()) + " Sequenzen aus der Aufzeichnung liegt vermutlich ein Sturz vor")
         
         y_pred_rf = pd.DataFrame(y_pred_rf)
-        y_pred_rf.replace(to_replace=0, value="No Fall", inplace=True)
+        y_pred_rf.replace(to_replace=0, value="Normal", inplace=True)
         y_pred_rf.replace(to_replace=1, value="Fall",inplace=True)
         
         st.write(y_pred_rf)
