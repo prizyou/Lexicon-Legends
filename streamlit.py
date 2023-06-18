da70ce186de61d160e5a902e5e0885609e7c0fc7
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-18 20:04:01 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -187,26 +187,26 @@ if uploaded_file is not None:
 st.title("Theorie:")
 st.write("""Sturzerkennung mit Machine Learning Modellen: 
 
-https://www.researchgate.net/publication/308467199_Fall_Detection_Using_Machine_Learning_Algorithms
-
 => Das k-NN und RF-Modell als Modelle, welche nach den wissenschaftlichen Papern / State of the Art am besten dazu geeignet sind, Stürze zu erkennen
     
- Activity Labels: 
+ Aktivitäts Labels: 
  
-- Standing
-- Walking
-- Jogging
-- Jumping
-- Stairs (UP)
-- Stairs (DOWN)
-- Sitting
-    
+- Stehen
+- Gehen
+- Springen
+- Treppe hochlaufen (UP)
+- Treppe herunterlaufen (DOWN)
+- Sitzen
+- Sich auf einen Stuhl setzen
+
+Wissenscahftliche Arbeiten/ Referenzen:
+
 https://www.sciencedirect.com/science/article/pii/S1877050918318398
 
 https://kth.diva-portal.org/smash/get/diva2:1230962/FULLTEXT01.pdf
 
-https://www.researchgate.net/publication/353576862_Latest_Research_Trends_in_Fall_Detection_and_Prevention_Using_Machine_Learning_A_Systematic_Review
+https://www.researchgate.net/publication/308467199_Fall_Detection_Using_Machine_Learning_Algorithms
 
-Zunaechst Datei Import ermoeglichen mit Kachel Drag&Drop Funktion
+https://www.researchgate.net/publication/353576862_Latest_Research_Trends_in_Fall_Detection_and_Prevention_Using_Machine_Learning_A_Systematic_Review
 
 https://jneuroengrehab.biomedcentral.com/articles/10.1186/s12984-021-00918-z""")
