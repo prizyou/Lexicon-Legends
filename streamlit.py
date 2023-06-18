990472e5a83a1182e6dfd1610162ae014dae89a8
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-18 19:59:47 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -184,4 +184,30 @@ if uploaded_file is not None:
         st.write(y_pred_rf.T)
 
 
-        st.title("Theorie:")
+st.title("Theorie:")
+st.write("""Fall Detection via Machine Learning: 
+
+https://www.researchgate.net/publication/308467199_Fall_Detection_Using_Machine_Learning_Algorithms
+
+=> The k-NN  were the
+    most reliable system (evidence from paper above)
+    
+ Activity Labels: 
+ 
+- Standing
+- Walking
+- Jogging
+- Jumping
+- Stairs (UP)
+- Stairs (DOWN)
+- Sitting
+    
+https://www.sciencedirect.com/science/article/pii/S1877050918318398
+
+https://kth.diva-portal.org/smash/get/diva2:1230962/FULLTEXT01.pdf
+
+https://www.researchgate.net/publication/353576862_Latest_Research_Trends_in_Fall_Detection_and_Prevention_Using_Machine_Learning_A_Systematic_Review
+
+Zunaechst Datei Import ermoeglichen mit Kachel Drag&Drop Funktion
+
+https://jneuroengrehab.biomedcentral.com/articles/10.1186/s12984-021-00918-z""")
