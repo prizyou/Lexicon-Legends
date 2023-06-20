11a21a05ea34bdc86974f5175e828d48d65df465
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-20 19:57:32 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -158,7 +158,7 @@ if uploaded_file is not None:
             st.write(featuresList)
 
         #Schätzungsdaten rausziehen
-        y_pred_knn = model_knn.predict(features[my_array[0]])
+        y_pred_knn = model_knn.predict(features[[my_array[0]]])
         y_pred_rf = model_rf.predict(features[my_array[0]])
 
         st.caption("Plot der Rohdaten:")
