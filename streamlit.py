d6a28730b091acbd0efdab71d7163861a9ae6921
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-20 19:58:47 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -155,11 +155,11 @@ if uploaded_file is not None:
         my_array = np.asarray(featuresList)
         
         if st.button('Features als Liste'):
-            st.write(featuresList)
+            st.write(featuresList[0])
 
         #Schätzungsdaten rausziehen
         y_pred_knn = model_knn.predict(features[[my_array[0]]])
-        y_pred_rf = model_rf.predict(features[my_array[0]])
+        y_pred_rf = model_rf.predict(features[[my_array[0]]])
 
         st.caption("Plot der Rohdaten:")
         st.line_chart(graph_daten)   
