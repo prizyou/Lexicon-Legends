a92ad3c50ecf8f0d1c3fcf628770824e0ad3b775
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-25 23:03:39 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -106,7 +106,7 @@ if uploaded_file is not None:
         #data_acc = data_acc.reset_index(inplace=True)
         #data_gyro = data_gyro.reset_index(inplace=True)
 
-        data_combine = pd.merge(data_acc, data_gyro, left_index=True, right_index=True)
+        data_combine = pd.concat(data_acc, data_gyro, axis=1, join='inner')
 
         st.write(data_combine)
 
