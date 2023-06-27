c5b88c90a6c49742eb355f4adc79818041a0e030
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-28 01:04:59 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -111,7 +111,7 @@ if uploaded_file is not None:
         data_or['index'] = 0
         data_gravity['index'] = 0
 
-        st.write(data_or)
+        st.write(data_gravity)
 
         for i in range(len(data_acc)):
             data_acc.iloc[i,3] = i
