d3d2a4978543517073ff5068f1e767b1314a4ac5
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-29 17:38:08 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -178,9 +178,9 @@ if uploaded_file is not None:
         st.caption("Plot der Rohdaten:")
         st.line_chart(graph_daten)
 
-        if st.selectbox("show"):
-
-
+        if st.checkbox("show"):
+            st.line_chart(graph_daten)
+            
         #Vorhersage Label in Modell
         st.header("Vorhersage Labels in KNN-Modell:")
         st.caption("In "+ str(y_pred_knn.sum()) + " der übertragenen " + str(data_combine['id'].unique().max()) + " Sequenzen aus der Aufzeichnung liegt vermutlich ein Sturz vor")
