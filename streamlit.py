79ec3a253b77b66478b3663bab5d410c0f441809
mAx <62607103+maxh7@users.noreply.github.com>
2023-07-02 14:36:04 +0200

    ed

--- streamlit.py
+++ streamlit.py
@@ -129,13 +129,13 @@ if uploaded_file is not None:
 
         id = 1
 
-        var1 = 100
+        var1 = 200
 
         for i in range(0, len(data_combine)):
             data_combine.iloc[i,14] = id
             
             if i >= var1: 
-                var1 = var1 + 100
+                var1 = var1 + 200
                 id+=1
 
         
