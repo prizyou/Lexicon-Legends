6fa70cc59f04bc2581fb9711d81fef59e33294a4
mAx <62607103+maxh7@users.noreply.github.com>
2023-07-04 09:47:33 +0200

    ed

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 st.title("Herzlich Willkommen zu FallDetector, die App zu ihrem Schutz")
 st.title("FallDetector hat den Anspruch Stürze zu erkennen und diese zu melden und von alltäglichen Verhaltensweisen abgrenzen zu können.")
 
@@ -9,4 +11,6 @@ st.box("")
 
 st.title("Die finale Idee")
 
-st.tilte("Ansprch an die App")
+st.tilte("Anspruch an die App")
+
+st.title("Business Case: Wie kann die FallDetector App kommerziell verwendet werden?")
