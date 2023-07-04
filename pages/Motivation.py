32b8893196dc23719b857730e1aefb34726af55f
mAx <62607103+maxh7@users.noreply.github.com>
2023-07-04 09:37:50 +0200

    ed

---
+++ pages/Motivation.py
@@ -0,0 +1,10 @@
+st.title("Herzlich Willkommen zu FallDetector, die App zu ihrem Schutz")
+st.title("FallDetector hat den Anspruch Stürze zu erkennen und diese zu melden und von alltäglichen Verhaltensweisen abgrenzen zu können.")
+
+st.title("Der Prozess zur Ideenentwicklung:")
+
+st.title("Unsere Ideen waren vielseitig...")
+st.box("Untergrunderkennung")
+st.box("")
+
+st.title("Die finale Idee")
