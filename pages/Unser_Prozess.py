6ea2b7910db3f1ce45a3e4403dd74763af91873b
mAx <62607103+maxh7@users.noreply.github.com>
2023-07-04 09:50:09 +0200

    ed

--- pages/Unser_Prozess.py
+++ pages/Unser_Prozess.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.title("Orientation an dem CrispCycle:")
+st.title("Orientation an dem Crisp-DM:")
+
+st.title("Business Understanding")
+st.title("Data Understanding")
+st.title("Data Preparation")
+st.title("Modeling")
+st.title("Evaluation")  
+st.title("Deployment")      
