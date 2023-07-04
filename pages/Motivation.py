eef2285e479bdc1b795d43957fb2366216a2bcec
mAx <62607103+maxh7@users.noreply.github.com>
2023-07-04 10:14:21 +0200

    ed

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -18,6 +18,14 @@ st.box("Sturzerkennung für ältere Menschen")
 st.title("Die finale Idee")
 st.write("")
 
-st.tilte("Anspruch an die App")
+st.tilte("Anspruch an die App:")
+st.markdown("- Stürze erkennen")
+st.markdown("- Alltägliche Verhaltensweisen von Stürzen abgrenzen") 
+st.markdown("- Stürze melden")
+st.markdown("- Datenaufzeichnung für jeden möglich sein")
+st.markdown("")
+
 
 st.title("Der Business Case: Wie kann die FallDetector App kommerziell verwendet werden?")
+st.write("Demographischer Wandel: Immer mehr ältere Menschen, die länger selbstständig leben wollen")
+st.write("")
