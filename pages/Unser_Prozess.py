e6677c7b9bc1ec29c08b7c5fa400909f0094d94b
mAx <62607103+maxh7@users.noreply.github.com>
2023-07-04 11:57:36 +0200

    ed

--- pages/Unser_Prozess.py
+++ pages/Unser_Prozess.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("Orientation an dem Crisp-DM:")
 
+st.image("pictures/5.PNG", caption='CRISP-DM',width=250,output_format="auto")
 st.title("Business Understanding")
 st.title("Data Understanding")
 st.title("Data Preparation")
