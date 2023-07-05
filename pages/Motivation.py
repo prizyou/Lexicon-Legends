6eecefc82c7009036277228a274d1f9a04f2e1ef
mAx <62607103+maxh7@users.noreply.github.com>
2023-07-05 09:01:35 +0200

    ed

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -7,13 +7,15 @@ st.title("Der Prozess zur Ideenentwicklung:")
 
 st.write("Unsere Ideen waren vielseitig...")
 if st.button("Untergrunderkennung für Fahrrad, Skateboard, E-Scooter"):
-    st.write("")
-if st.button("Risikoprofil für Autofahrer"):
-    st.write("")
+    st.write("Ermittlung der Untergrundbeschaffenheit durch Vibration")
+    st.write("Wir fahren über eine gewisse Art Untergrund und Labeln dann die Daten") 
+    st.write("Unsere App gibt dann aus, wie schnell eine Person fahren sollte bzw. Drosselt die Geschwindigkeit")
+if st.button("Risikoprofil für Autofahrer/Verkehrsteilnehmer"):
+    st.write("Risikoeinschätzung des Fahrverhaltens, Aufstellung individueller Fahrprofile")
 if st.button("Herzfrequenz bei sportlichen Aktivitäten: Krankheitsbild erkennen"):
-    st.write("")
+    st.write("Marker identifizieren, die auf ein Krankheitsbild hindeuten")
 if st.button("Sturzerkennung für ältere Menschen"):
-    st.write("")
+    st.write("Beschleunigungssensor, Gyroskop, Orientation und Gravity Sensoren als Ausgangspunkt für die Erkennung und Warnung von Stürzen")
 
 st.title("Die finale Idee")
 st.write("")
